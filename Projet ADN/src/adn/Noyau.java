f9aded0483240d222905de481c2998a0106670a0
AnissaBelarbia <b.anissa9@outlook.fr>
2020-02-01 19:16:38 +0100

    Début du package adn

---
+++ Projet ADN/src/adn/Noyau.java
@@ -0,0 +1,12 @@
+package adn;
+
+public class Noyau {
+
+	private ArrayList<Chromosome> chromosomes;
+	private Centromere centromere;
+	private Fuseaumitotique fuseau;
+	public Noyau() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
