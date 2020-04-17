6a3947fdba53e9579ca08055c94037bb5ac81c7c
danyUCP <danielfrancois1@yahoo.fr>
2020-04-17 20:55:01 +0200

    34e commit : commit final

---
+++ Projet ADN/readme.txt
@@ -0,0 +1,4 @@
+Démarrage du programme depuis Eclipse.
+
+Pour lancer le programme, aller dans le package test, puis la classe Main qui contient la méthode main.
+Cliquer sur Main run.
---
+++ Projet ADN/src/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+import ihm.Fenetre;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Fenetre adn = new Fenetre();
+		adn.setVisible(true);
+
+		
+	}
+
+}
